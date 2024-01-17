ba0b4a516fd2be305c4a59afb8cce39dcadd2909
Rinz0x0cruZ <rinz0x0cruz@gmail.com>
2024-01-17 14:32:30 -0500

    Git Pusher Update

---
+++ gitpush.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+read -p "[+] Path: " path
+read -p "[+] Files if any: " files
+read -p "[+] Comment: " comment
+
+cd "$path" || { echo "Error changing directory"; exit 1; }
+
+git add "$files"
+
+read -p "[+] Need a status check? " chk
+
+case "$chk" in
+    [Yy]* ) git status ;;
+    [Nn]* ) echo "continuing....." ;;
+    * ) echo "[+] Please answer in yes or no" ;;
+esac
+
+git commit -m "$comment"
+git push
+
+case "$?" in
+    0) echo "[+] Successfully Pushed" ;;
+    *) echo "[+] Error Occurred" ;;
+esac
